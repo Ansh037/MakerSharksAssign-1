ff002126d2f6e10c34512154b3ef9a97389ad9fe
Ansh037 <145255134+Ansh037@users.noreply.github.com>
2024-06-19 23:18:21 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,46 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+  public Long getId(){
+    return id;
+  }
+
+  public void setId(Long ID){
+    this.id=ID;
+  }
+
+  public String getUsername(){
+    return username;
+  }
+
+  public void setUsername(String newUserName){
+    this.username=newUserName;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public void setId(String userEmail){
+    this.email=userEmail;
+  }
+
+  //no get method added for password method for privacy purpose
+  public void setPassword(String userPassword){
+    this.password=userPassword;
+  }
+  
+
+    
+}
